a84884dde7be07e35f800f7c04d324ee35b5bd26
Alejandro Gonzalez Romero <me@algorodev.com>
2024-05-12 19:56:49 +0100

    feat: added cors setup to server and dotenv setup for environment variables

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
+import cors from 'cors'
+import dotenv from 'dotenv'
 import express from 'express'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Hello World! Welcome to InstantBuzz API.')
 })
 
-app.listen(3000, () => {
-	console.log('Server is running on http://localhost:3000')
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
 })
