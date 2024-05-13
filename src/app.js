535d8e6461715a1a34a699ec5341002ed67505e8
Alejandro Gonzalez Romero <me@algorodev.com>
2024-05-13 17:10:53 +0100

    feat: set basic api configuration

--- src/app.js
+++ src/app.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
 	res.send('Hello World! Welcome to InstantBuzz API.')
