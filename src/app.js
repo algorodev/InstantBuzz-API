b1faa8199b69ce8f82ea36a1c734828786a3ecb5
Alejandro Gonzalez Romero <me@algorodev.com>
2024-05-13 17:39:50 +0100

    feat: added user model

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import { connectDB } from './config/dbConnection.js'
+import User from './models/User.js'
 
 dotenv.config()
 
@@ -17,8 +18,19 @@ app.get('/', (req, res) => {
 	res.send('Hello World! Welcome to InstantBuzz API.')
 })
 
-app.listen(PORT, () => {
-	console.log(`Server is running on http://localhost:${PORT}`)
+app.post('/users', async (req, res) => {
+	const user = new User(req.body)
+
+	try {
+		await user.save()
+		res.status(201).send(user)
+	} catch (error) {
+		res.status(500).send(error)
+	}
 })
 
 connectDB()
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
